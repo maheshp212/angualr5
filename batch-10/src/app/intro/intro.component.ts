9c13155e4f0009fd06005e5b1c0d92bceb9b39f2
mahesh <mahesh.perla@gritinnovatino.com>
2017-12-12 08:33:47 +0530

    intro component added

---
+++ batch-10/src/app/intro/intro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+})
+export class IntroComponent {
+
+  constructor() { }
+
+}
