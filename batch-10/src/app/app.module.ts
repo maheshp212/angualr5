55514a276fa932973e6bb8f3de10d4923719317a
mahesh <mahesh.perla@gritinnovatino.com>
2017-12-13 08:26:09 +0530

    addign the templates

--- batch-10/src/app/app.module.ts
+++ batch-10/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { HomeComponent } from './home/home.component';
+import { VariablesComponent } from './variables/variables.component';
 
+const appRoutes:Routes = [
+  {path:'', component:HomeComponent},
+  {path:'intro', component:IntroComponent},
+  {path:'varibales', component:VariablesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    HomeComponent,
+    VariablesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
