a3e747eb844e81319a8085758acae2f26aaf14e9
mahesh <mahesh.perla@gritinnovatino.com>
2018-01-20 15:00:11 +0530

    adding batch11

---
+++ batch11/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
