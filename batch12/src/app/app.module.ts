706c58b1ce058a8f875de7ba220e4ae88bfb3efe
mahesh <mahesh.perla@gritinnovatino.com>
2018-03-01 15:23:16 +0530

    extra files added

--- batch12/src/app/app.module.ts
+++ batch12/src/app/app.module.ts
@@ -1,11 +1,43 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
+import {FormsModule} from '@angular/forms'
 
-import { AppComponent }  from './app.component';
+
+
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { VariablesComponent } from './variables/variables.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { FormStatesComponent } from './form-states/form-states.component';
+import { FormsComponent } from './forms/forms.component';
+import { AttrDirective } from './attr.directive';
+
+
+const routes:Routes = [
+  {path:'intro', component:IntroComponent},
+  {path:'variables', component:VariablesComponent},
+  {path:'directives', component:DirectivesComponent},
+  {path:'form-states', component:FormStatesComponent},
+  {path:'forms', component:FormsComponent},
+]
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    VariablesComponent,
+    DirectivesComponent,
+    FormStatesComponent,
+    FormsComponent,
+    AttrDirective
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
